2b580977626040c98edc8d32f78b4ef888bb9b99
tikhiy <mechtapatsana@yandex.ru>
2026-02-25 10:42:18 +0100

    ajout test POST

--- projet/test/load/locustfile.py
+++ projet/test/load/locustfile.py
@@ -1,8 +1,19 @@
 from locust import HttpUser, task, between
 
+
 class GoProjectUser(HttpUser):
+
     wait_time = between(1, 3)
+
+        # .........GET
+
     @task
-    def load_homepage(self):
-        
-        self.client.get("/api/count")
+    def load_count(self):
+        self.client.get('/api/count')
+
+        # .........POST
+
+    @task
+    def increment_counter(self):
+
+        self.client.post('/api/increment')
